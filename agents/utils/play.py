ed9e5e2a329f0646cd0323e5bc0d6fcc54d2940a
Sohail Rajabi <srajabi@gmail.com>
2019-05-03 19:13:41 -0700

    Subrepo n-tictactoe environment. Update play script to debug environment.

--- agents/utils/play.py
+++ agents/utils/play.py
@@ -1,37 +1,15 @@
 import gym
-import gym_maze
+import gym_ntictactoe
 import time
-from gym.utils.play import play
 
-env = gym.make('maze-v0')
-
-keys_to_action = {
-            (ord('w'),):0,
-            (ord('s'),):1,
-            (ord('d'),):2,
-            (ord('a'),):3
-        }
-
-#play(env, keys_to_action=keys_to_action)
-
-
-print(env.observation_space.low)
-print(env.observation_space.high)
-
-env = gym.make('FrozenLake8x8-v0')
-
-print(env.observation_space.n)
-
+env = gym.make('tictactoe-v0')
 
 done = False
-
 env.reset()
 while not done:
-    #time.sleep(1)
     env.render()
-    #action = env.action_space.sample()
-
-    action = int(input('action'))
+    str = input("Input Action: 'Player Move',  where Player is {-1, 1} and move is flattened index: \n")
+    action = tuple(map(int, str.split(' ')))
     print(action)
     obv, reward, done, info = env.step(action)
 
